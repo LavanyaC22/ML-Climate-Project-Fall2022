c4792824d09d87fb2b1510ec4b021a85e7d9ce81
LavanyaC22 <68581239+LavanyaC22@users.noreply.github.com>
2022-11-28 04:47:09 -0500

    Updated data download file
    Now covers all countries, years and variables available based on decided regions

--- era5_data_download.py
+++ era5_data_download.py
@@ -1,15 +1,64 @@
 import cdsapi
+import pandas as pd
 
 c = cdsapi.Client()
 
-c.retrieve(
-    'sis-agrometeorological-indicators',
-    {
-        'format': 'zip',
-        'variable': '2m_temperature',
-        'statistic': '24_hour_mean',
-        'year': '2020',
-        'month': '01',
-        'day': '01',
-    },
-    'download.zip')
+countries = pd.read_csv("countries.csv")
+variables = [
+                'cloud_cover', 'snow_thickness_lwe', 'snow_thickness', 'vapour_pressure',
+                '2m_temperature', '10m_wind_speed', '2m_dewpoint_temperature'
+            ]
+years = [
+                    '1979', '1980', '1981',
+                    '1982', '1983', '1984',
+                    '1985', '1986', '1987',
+                    '1988', '1989', '1990',
+                    '1991', '1992', '1993',
+                    '1994', '1995', '1996',
+                    '1997', '1998', '1999',
+                    '2000', '2001', '2002',
+                    '2003', '2004', '2005',
+                    '2006', '2007', '2008',
+                    '2009', '2010', '2011',
+                    '2012', '2013', '2014',
+                    '2015', '2016', '2017',
+                    '2018', '2019', '2020',
+                ]
+
+for country in list(countries["Country"]):
+    for variable in variables:
+        for year in years:
+            c.retrieve(
+                'sis-agrometeorological-indicators',
+                {
+                    'format': 'zip',
+                    'variable': variable,
+                    'statistic': '24_hour_mean',
+                    'year': year,
+                    'month':  [
+                        '01', '02', '03',
+                        '04', '05', '06',
+                        '07', '08', '09',
+                        '10', '11', '12',
+                    ],
+                    'day': [
+                        '01', '02', '03',
+                        '04', '05', '06',
+                        '07', '08', '09',
+                        '10', '11', '12',
+                        '13', '14', '15',
+                        '16', '17', '18',
+                        '19', '20', '21',
+                        '22', '23', '24',
+                        '25', '26', '27',
+                        '28', '29', '30',
+                        '31',
+                    ],
+                    'area': [
+                        int(countries.loc[countries["Country"] == country, "North"]), 
+                        int(countries.loc[countries["Country"] == country, "West"]), 
+                        int(countries.loc[countries["Country"] == country, "South"]), 
+                        int(countries.loc[countries["Country"] == country, "East"]), 
+                    ],
+                },
+                country+"_"+variable+"_"+year+".zip")
