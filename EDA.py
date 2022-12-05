ac482f7347c396e96b95359307def8c26cb8a6ff
LavanyaC22 <68581239+LavanyaC22@users.noreply.github.com>
2022-12-04 22:43:35 -0500

    Add files via upload

---
+++ EDA.py
@@ -0,0 +1,29 @@
+#Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Load data file
+df = pd.read_csv("yield_and_climate_data.csv")
+
+#Dataset info
+df.info()
+print(df.describe().round(2))
+
+## Data cleaning
+
+#Dealing with missing values - remove rows without yield data and/or climate data
+
+
+#Dummy varibles for countries
+
+
+## Distribution of vatiables
+
+
+## Multivariate analysis
+
+
+## Correlation map
+
